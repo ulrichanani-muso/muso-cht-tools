1e1ec96ecf17a5af6490520fd962843aba2caf36
Ulrich Anani <uanani@musohealth.org>
2022-11-29 04:18:12 +0000

    Update auth with jwt

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-// import * as jwt from 'jsonwebtoken'
+import * as jwt from 'jsonwebtoken'
 import prisma from '../../../../lib/prisma.ts'
 
 export const authOptions = {
@@ -12,17 +12,17 @@ export const authOptions = {
     }),
   ],
   adapter: PrismaAdapter(prisma),
-  // session: {
-  //   strategy: 'jwt',
-  // },
-  // jwt: {
-  //   secret: process.env.NEXTAUTH_SECRET,
-  //   encode: async ({ secret, token }) => jwt.sign({ ...token, userId: token.id }, secret, {
-  //     algorithm: 'HS256',
-  //     expiresIn: 30 * 24 * 60 * 60, // 30 days
-  //   }),
-  //   decode: async ({ secret, token }) => jwt.verify(token, secret, { algorithms: ['HS256'] }),
-  // },
+  session: {
+    strategy: 'jwt',
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+    encode: async ({ secret, token }) => jwt.sign({ ...token, userId: token.id }, secret, {
+      algorithm: 'HS256',
+      // expiresIn: 30 * 24 * 60 * 60, // 30 days
+    }),
+    decode: async ({ secret, token }) => jwt.verify(token, secret, { algorithms: ['HS256'] }),
+  },
 }
 
 export default NextAuth(authOptions)
