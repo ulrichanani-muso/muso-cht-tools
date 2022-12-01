f6fa858fdae0a31a1d370daf903b293e4fdd4c15
Ulrich Anani <uanani@musohealth.org>
2022-12-01 02:31:35 +0000

    Setup lucid and user model

---
+++ apps/api/app/Models/User.ts
@@ -0,0 +1,28 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public uid: string
+
+  @column()
+  public email: string
+
+  @column()
+  public avatar: string
+
+  @column()
+  public name: string
+
+  @column({ serializeAs: null })
+  public password?: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
